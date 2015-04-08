1550d1a2c215896a92014a8ed5df05425e3e6bee
Andrew Hope <hope.andrew08@gamil.com>
2015-04-07 18:51:35 -0700

    add static pages controller and view and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root to: "sessions#new"
+  root to: "static_pages#root"
 
   resources :users
   resource :session, only: [:new, :create, :destroy]
+  resources :static_pages, only: [:root]
 
   namespace :api, defaults: { format: :json } do
     resources :photos
