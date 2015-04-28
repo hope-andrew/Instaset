df1f329cb769c2bf142e6316c0de62ddf820ffe1
Andrew Hope <hope.andrew08@gamil.com>
2015-04-28 16:40:49 -0700

    Add Comments in photo index view, unstyled

--- app/assets/javascripts/views/photo_item.js
+++ app/assets/javascripts/views/photo_item.js
@@ -1,15 +1,27 @@
-Instaset.Views.PhotoItem = Backbone.View.extend({
+Instaset.Views.PhotoItem = Backbone.CompositeView.extend({
   template: JST["items/photo"],
 
   tagName: "li",
 
   initialize: function() {
     this.listenTo(this.model, "sync add", this.render);
+    this.renderComments();
   },
 
   render: function() {
     var renderedContent = this.template({ photo: this.model});
     this.$el.html(renderedContent);
+    this.attachSubviews();
     return this;
-  }
+  },
+
+  renderComments: function() {
+    this.model.comments().each(this.addComment.bind(this));
+  },
+
+  addComment: function(comment) {
+    var view = new Instaset.Views.CommentItem({model: comment});
+    this.addSubview("ul.comments-index-list", view);
+  },
+
 });
